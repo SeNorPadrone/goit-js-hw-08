af8408706ecab74b26ef6870b015d1d503bcf862
SeNorPadrone <90000708+SeNorPadrone@users.noreply.github.com>
2022-02-22 22:22:43 +0200

    2
    2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+form.addEventListener('submit', onSubmitBtn);
+form.addEventListener('input', throttle(onTextInput, 1000));
+
+populateText();
+
+function onSubmitBtn(e) {
+  e.preventDefault(); //Убираем перезагрузку страницы
+  console.log('Send');
+  e.currentTarget.reset(); //Очищяем поля формы
+  localStorage.removeItem(STORAGE_KEY);
+}
+function onTextInput(e) {
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateText() {
+  const savedText = localStorage.getItem(STORAGE_KEY);
+  if (savedText) {
+    form.email.value = JSON.parse(savedText).email;
+    form.message.value = JSON.parse(savedText).message;
+  }
+}
